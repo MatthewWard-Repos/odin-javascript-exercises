10ef2a40c759b653af1a3fd037bd436a212cf236
Matthew Ward <175315171+MatthewWard-Repos@users.noreply.github.com>
2026-03-04 12:10:00 +0000

    Write and test factorial().

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,6 +1,15 @@
-const factorial = function() {
-
+const factorial = function (n) {
+  //   if (n < 0 || n % 1 !== 0 || typeof n !== "number") {
+  //     return undefined;
+  //   } else if (n === 1 || n === 0) {
+  //     return 1;
+  //   } else {
+  //     return n * factorial(n - 1);
+  //   }
+  if (!Number.isInteger(n) || n < 0) return;
+  if (n === 0) return 1;
+  return n * factorial(n - 1);
 };
-
+console.log(factorial(4));
 // Do not edit below this line
 module.exports = factorial;
