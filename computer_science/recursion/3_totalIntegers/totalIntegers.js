5c1761a35117b194a32656c9945ec39bfbdea3c0
Matthew Ward <175315171+MatthewWard-Repos@users.noreply.github.com>
2026-03-09 14:16:55 +0000

    Create solution for totalIntegers recursive function

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,31 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (container) {
+  if (typeof container !== "object" || container === null) return undefined;
+
+  let integers = 0;
+  const values = Array.isArray(container)
+    ? container
+    : Object.values(container);
+
+  values.forEach((value) => {
+    if (Number.isInteger(value)) integers++;
+    else if (typeof value === "object" && value !== null) {
+      integers += totalIntegers(value);
+    }
+  });
+
+  return integers;
 };
-  
+
+totalIntegers([
+  NaN,
+  [[{}], 555],
+  "444",
+  [],
+  74.0,
+  undefined,
+  [[() => {}], [4], Infinity, [[[], -44.0], [null, "-4"], NaN[[]], 6]],
+  () => {},
+  [[], [-Infinity, ["4"], [4.7, -46.7], NaN]],
+]);
 // Do not edit below this line
 module.exports = totalIntegers;
