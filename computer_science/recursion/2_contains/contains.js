2721dd11c47cf066fdefe1c0faeec44da9bb4a5a
Matthew Ward <175315171+MatthewWard-Repos@users.noreply.github.com>
2026-03-09 14:16:26 +0000

    Create solution for contains recursive function.

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,26 @@
-const contains = function() {
-  
+const contains = function (obj, val) {
+  let values = Object.values(obj);
+  if (values.includes(val)) return true;
+  for (value of values) {
+    if (typeof value === "object" && value) {
+      if (contains(value, val)) return true;
+    }
+  }
+  return false;
 };
-  
+
+// const contains = function (object, searchValue) {
+//   const values = Object.values(object);
+//   if (values.includes(searchValue)) return true;
+
+//   const nestedObjects = values.filter(
+//     (value) => typeof value === "object" && value !== null,
+//   );
+
+//   return nestedObjects.some((nestedObject) =>
+//     contains(nestedObject, searchValue),
+//   );
+// };
+
 // Do not edit below this line
 module.exports = contains;
