f4b1cfbd86fd86d8a41b1325bd939b69c693c1b2
Matthew Ward <175315171+MatthewWard-Repos@users.noreply.github.com>
2026-03-10 17:46:46 +0000

    Solution to permutation problem.

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,20 @@
-const permutations = function() {
-  
+const permutations = function (arr) {
+  let result = [];
+
+  if (arr.length === 0) {
+    result.push(arr);
+    return result;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    const first = arr[i];
+    const remaining = arr.toSpliced(i, 1);
+    const perms = permutations(remaining);
+    perms.forEach((perm) => {
+      result.push([first, ...perm]);
+    });
+  }
+  return result;
 };
-  
 // Do not edit below this line
 module.exports = permutations;
