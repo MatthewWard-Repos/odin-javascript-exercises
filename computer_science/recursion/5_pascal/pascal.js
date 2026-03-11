8daf48574b49231500def28414560200ddf3a197
Matthew Ward <175315171+MatthewWard-Repos@users.noreply.github.com>
2026-03-11 10:14:37 +0000

    Create solution for pascal function.

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,14 @@
-const pascal = function() {
-  
+const pascal = function (n) {
+  if (n === 1) {
+    return [1];
+  }
+  const previousRow = pascal(n - 1);
+  const middle = previousRow
+    .slice(0, -1)
+    .map((num, i) => num + previousRow[i + 1]);
+
+  return [1, ...middle, 1];
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
